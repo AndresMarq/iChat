260dbe0b192ed75e56bd748eb8ec82e09b57f45b
AndresMarq <70426525+AndresMarq@users.noreply.github.com>
2022-08-13 22:26:32 -0600

    Finished UI for log in screen

--- iChat/Controller/LogInViewController.swift
+++ iChat/Controller/LogInViewController.swift
@@ -8,6 +8,62 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    private let emailTextField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Email"
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 15
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.gray.cgColor
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.leftViewMode = .always
+        return field
+    }()
+
+    private let passwordTextField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Password"
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .done
+        field.layer.cornerRadius = 15
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.gray.cgColor
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.leftViewMode = .always
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let logInButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(logInTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private let registerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Don't have an account yet?"
+        label.font = .systemFont(ofSize: 14, weight: .light)
+        return label
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Register!", for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 14, weight: .light)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(registerTapped(_:)), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,14 +84,83 @@ class LogInViewController: UIViewController {
         view.addSubview(imageView)
         
         // Add e-mail text field
-        let emailTextFrame = CGRect(
-            x: view.width * 0.05,
-            y: imageView.bottom + 20,
+        emailTextField.frame = CGRect(
+            x: view.width * 0.1,
+            y: imageView.bottom + 100,
             width: view.width * 0.8,
             height: 50
         )
-        let emailTextField = InputFieldView(frame: emailTextFrame)
-        emailTextField.addPlaceholder(placeholder: "Email")
+        emailTextField.delegate = self
         view.addSubview(emailTextField)
+        
+        // Add password text field
+        passwordTextField.frame = CGRect(
+            x: view.width * 0.1,
+            y: emailTextField.bottom + 30,
+            width: view.width * 0.8,
+            height: 50
+        )
+        passwordTextField.delegate = self
+        view.addSubview(passwordTextField)
+        
+        // Add Log In Button
+        logInButton.frame = CGRect(
+            x: (view.width / 2) - (view.width * 0.125),
+            y: passwordTextField.bottom + 30,
+            width: view.width * 0.25,
+            height: 50
+        )
+        view.addSubview(logInButton)
+        
+        // Add register label and button
+        registerLabel.frame = CGRect(
+            x: view.width * 0.1,
+            y: logInButton.bottom + 30,
+            width: view.width * 0.45,
+            height: 30
+        )
+        view.addSubview(registerLabel)
+        
+        registerButton.frame = CGRect(
+            x: registerLabel.right,
+            y: logInButton.bottom + 30,
+            width: view.width * 0.2,
+            height: 30
+        )
+        view.addSubview(registerButton)
+    }
+    
+    @objc private func logInTapped() {
+        // Ensures valid e-mail and password fields
+        guard let email = emailTextField.text, let password = passwordTextField.text, !email.isEmpty, !password.isEmpty else {
+            self.view.endEditing(true)
+            logInError()
+            return
+        }
+        
+        // Firebase log in
+    }
+    
+    private func logInError() {
+        let ac = UIAlertController(title: "Error", message: "Invalid Email or Password", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+        present(ac, animated: true)
+    }
+    
+    @objc private func registerTapped(_ sender: UIButton) {
+        let viewController = RegisterViewController()
+        navigationController?.pushViewController(viewController, animated: false)
+    }
+}
+
+// Ensure textfields return and continue properly
+extension LogInViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            passwordTextField.becomeFirstResponder()
+        } else if textField == passwordTextField {
+            logInTapped()
+        }
+        return true
     }
 }
---
+++ iChat/Controller/RegisterViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RegisterViewController.swift
+//  iChat
+//
+//  Created by Andres Marquez on 2022-08-13.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Create Account"
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
